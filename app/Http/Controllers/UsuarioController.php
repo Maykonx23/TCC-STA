9ddff963032222b6d18c80710ae573bb7b31e645
Maykonx23 <802.140@alunos.unigran.br>
2021-07-05 23:46:58 -0400

    Entidades

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\endereco;
 use App\Models\usuario;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
-
-{public function index(){
-        $usuarios = endereco::all();
-        return  view('usuarios.index', ['usuarios' => $usuarios]);
-    }
+{
     
 }
